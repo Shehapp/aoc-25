e4c16aae70de491508ea4fa884065b99cfb6ae4d
shehap eldin <shehap-eldin.essam.ext@siemens.com>
2025-12-04 13:18:42 +0200

    Add Day 4 Part 1 solution

---
+++ day_0x04/PrintingDepartmentPart1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int dx[] = {0, 1, 0, -1, 1, -1, -1, 1};
+int dy[] = {1, 0, -1, 0, -1, 1, -1, 1};
+
+signed main()
+{
+    std::ifstream infile("input.txt");
+    vector<string> lines;
+    string line;
+    while (getline(infile, line))
+        lines.push_back(line);
+
+    auto isRollsOfPaper=[&](int x, int y)->bool
+    {
+        return (x < line.size() and y < lines.size() and lines[y][x] == '@');
+    };
+    int res{0};
+    for(int i = 0; i < lines.size(); i++)
+    {
+        for(int j = 0; j < line.size(); j++)if(lines[i][j] == '@')
+        {
+            int cur{0};
+            for(int k = 0; k < 8; k++)
+                cur += isRollsOfPaper(j + dx[k], i + dy[k]);
+
+            res += (cur < 4);
+        }
+    }
+
+    printf("\n %d \n", res);
+
+    return 0;
+}
